9b44c716c567ba202cb77fc5cb2f4574b8cf3350
Sahil Bhankhor <sahil10052005@gmail.com>
2026-06-28 14:22:25 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,42 @@
 
 int main()
 {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
   while (true)
   {
     std::cout << "$ ";
 
     std::string command;
     std::getline(std::cin, command);
+
     if (command == "exit")
     {
       break;
     }
-
-    if (command.substr(0, 4) == "echo")
+    else if (command.substr(0, 4) == "echo")
     {
       std::cout << command.substr(5) << std::endl;
     }
-
-    if (command.substr(0, 4) == "type")
+    else if (command.substr(0, 4) == "type")
     {
-      if (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type")
+      std::string cmd = command.substr(5);
+
+      if (cmd == "echo" || cmd == "exit" || cmd == "type")
       {
-        std::cout << command.substr(5) << " is a shell builtin " << std::endl;
+        std::cout << cmd << " is a shell builtin" << std::endl;
       }
       else
       {
-        std::cout << command.substr(5) << ": not found " << std::endl;
+        std::cout << cmd << ": not found" << std::endl;
       }
     }
+    else
+    {
+      std::cout << command << ": command not found" << std::endl;
+    }
   }
+
+  return 0;
 }
