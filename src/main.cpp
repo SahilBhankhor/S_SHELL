c667b86c76946f14b61866da1e14e8995baabb06
Sahil Bhankhor <sahil10052005@gmail.com>
2026-06-28 14:29:38 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <sstream>
 
 int main()
 {
@@ -17,23 +19,44 @@ int main()
     {
       break;
     }
+
     else if (command.substr(0, 4) == "echo")
     {
       std::cout << command.substr(5) << std::endl;
     }
+
     else if (command.substr(0, 4) == "type")
     {
       std::string cmd = command.substr(5);
-
+      bool isBuiltIn = false;
       if (cmd == "echo" || cmd == "exit" || cmd == "type")
       {
+        isBuiltIn = true;
         std::cout << cmd << " is a shell builtin" << std::endl;
       }
+
+      if (isBuiltIn == false)
+      {
+        std::string path = getenv("PATH");
+        std::stringstream ssPath(path);
+        std::string dictionary;
+        char delimiter = ';';
+        while (std::getline(ssPath, dictionary, delimiter))
+        {
+          std::string fullPath = dictionary + "/" + cmd;
+          if (access(fullPath.c_str(), X_OK) == 0)
+          {
+            std::cout << cmd << " is " << fullPath << std::endl;
+          }
+        }
+      }
+
       else
       {
         std::cout << cmd << ": not found" << std::endl;
       }
     }
+
     else
     {
       std::cout << command << ": command not found" << std::endl;
